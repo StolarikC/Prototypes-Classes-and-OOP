534b912a99445a6d5c53336a83fb33f615735c80
StolarikC <93167335+StolarikC@users.noreply.github.com>
2023-02-17 09:41:23 -0500

    Add files via upload

---
+++ Object Oriented Programming/Factory Function/app.js
@@ -0,0 +1,168 @@
+// // COLORS: "FUNCTION FACTORY"
+// function hex(r, g, b) {
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+// function rgb(r, g, b) {
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// // hex(255,100,25);
+// // rgb(255,100,25);
+// // "#ff6419"
+// // "rgb(255,100,25)"
+
+// function makeColor(r, g, b) {
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function () {
+//         const { r, g, b } = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+//     };
+//     color.hex = function () {
+//         const { r, g, b } = this;
+//         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//     }
+//     return color;
+// }
+
+// const firstColor = makeColor(35, 255, 150);
+// // firstColor.rgb();
+// // firstColor.hex();
+
+//
+//
+
+// // CONSTRUCTOR FUNCTIONS!
+// // *****************************************
+// // ** The "new" operator is crucial here! **
+// // *****************************************
+// // 1. Create a blank, plain JavaScript object;
+// // 2. Links (sets the constructor of) this object to another object;
+// // 3. Passes the newly created object from Step 1 as the this context;
+// // 4. Returns this if the function doesn't return its own object.
+
+// function Color(r, g, b) {
+//     const o = {}
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+// }
+
+// Color.prototype.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// Color.prototype.hex = function () {
+//     const { r, g, b } = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// Color.prototype.rgba = function (a = 1.0) {
+//     const { r, g, b, a } = this;
+//     return `rgba(${r}, ${g}, ${b}, ${a})`;
+// }
+
+// const color1 = new Color(91, 94, 98);
+// const color2 = new Color(0, 0, 0);
+
+// 
+// 
+
+// ************************************************
+// JavaScript Classes!
+// ************************************************
+
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+        this.calcHSL();
+    }
+    innerRGB() {
+        const { r, g, b } = this;
+        return `${r}, ${g}, ${b}`;
+    }
+    rgb() {
+        return `rgb(${this.innerRGB()})`;
+    }
+    rgba(a = 1.0) {
+        return `rgba(${this.innerRGB()}, ${a})`;
+    }
+    hex() {
+        const { r, g, b } = this;
+        return (
+            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+        );
+    }
+    hsl() {
+        const { h, s, l } = this;
+        return `hsl(${h},100%, ${l}%)`;
+    }
+    fullySaturated() {
+        const { h, l } = this;
+        return `hsl(${h},100%, ${l}%)`;
+    }
+    opposite() {
+        const { h, s, l } = this;
+        const newHue = (h + 180) % 360;
+        return `hsl(${newHue},${s}%, ${l}%)`;
+    }
+    calcHSL() {
+        // Make r, g, and b fractions of 1
+        const { r, g, b } = this;
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        // Find the greatest and smallest channel values
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0;
+        s = 0;
+        l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            // Red is max
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            // Green is max
+            h = (b - r) / delta + 2;
+        else
+            // Blue is max
+            h = (r - g) / delta + 4;
+
+        h = Math.round(h * 60);
+
+        // Make negative hues positive behind 360
+        if (h < 0) h += 360;
+        // Calculate lightness
+        l = (cmax + cmin) / 2;
+
+        // Calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        // Multiply  l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+
+        this.h = h;
+        this.s = s;
+        this.l = l;
+    }
+}
+
+    // const c1 = new Color(91, 94, 98, "sibling grey");
+
+
+
+// ************************************************
+// "MORE CLASSES PRACTICE!"
+// ************************************************
+
+// function from external party that COLT COPIED
